5e881b4227f1dd0fca417c9d1e129b0a67af45cc
V1daa <mrjokerekoj@gmail.com>
2024-05-23 16:10:15 +0200

    Dont forget to add animations

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,25 @@
 "use client";
+
 import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
-const images = ['1.avif', '2.avif', '3.avif', '4.avif']
-
+const images = ["1.avif", "2.avif", "3.avif", "4.avif"];
 
 export default function Home() {
   const [dark, setDark] = useState(false);
   const [isHovered, setIsHovered] = useState(false);
   const [page, setPage] = useState(0);
-  const [image, setImage] = useState(null);
+  const [image, setImage] = useState("1.avif");
 
   useEffect(() => {
     const intervalId = setInterval(() => {
       //@ts-ignore
-        setImage(images[Math.floor(Math.random() * images.length)]);
-    }, 8000)
-    
+      setImage(images[Math.floor(Math.random() * images.length)]);
+    }, 2000);
+
     return () => clearInterval(intervalId);
-}, [])
+  }, []);
 
   const handleMouseEnter = () => {
     setIsHovered(true);
@@ -31,7 +31,7 @@ export default function Home() {
 
   const handleScroll = () => {
     const position = window.scrollY;
-    if (position > 500) {
+    if (position > 500 && position < 1000) {
       setPage(1);
     } else if (position > 1000) {
       setPage(2);
@@ -51,13 +51,13 @@ export default function Home() {
   return (
     <main className={`${dark ? "dark" : ""}`}>
       <div className="dark:bg-black dark:text-white">
-        <section className="main-grid items-center">
-        <button
-          className="fixed top-0 w-14 h-14 bg-black text-white rounded-[50%] dark:bg-white dark:text-black m-4 hover:rounded-md transition-all duration-200"
-          onClick={() => setDark(!dark)}
-        >
-          DARK
-        </button>
+        <section className="main-grid items-center " id="main">
+          <button
+            className="z-50 fixed top-0 w-14 h-14 bg-black text-white rounded-[50%] dark:bg-white dark:text-black m-4 hover:rounded-md transition-all duration-200"
+            onClick={() => setDark(!dark)}
+          >
+            DARK
+          </button>
           <div
             className={`w-full h-full flex items-center flex-col justify-center transition-all ${
               isHovered ? "opacity-0" : ""
@@ -79,16 +79,24 @@ export default function Home() {
             </div>
             <div className="pt-10 flex flex-row gap-[30vw] w-full ml-10">
               <div className="flex flex-col gap-2">
-              <h1 className="uppercase">Dmytro Volianskyi</h1>
-              <h1 className="text-gray-400">Full-Stack Developer & UI|UX Designer</h1>
+                <h1 className="uppercase">Dmytro Volianskyi</h1>
+                <h1 className="text-gray-400">
+                  Full-Stack Developer & UI|UX Designer
+                </h1>
               </div>
-              <h1 className=" max-w-64 text-sm uppercase pt-16">Every project is a challenge to do better i have ever done</h1>
+              <h1
+                className="text max-w-64 text-sm uppercase pt-16 text"
+                id="scramble"
+              >
+                Every project is a challenge to do better i have ever done
+              </h1>
             </div>
           </div>
           <div className="h-[100vh] bg-gray-200 w-[1px]"></div>
-          <div className="flex flex-col gap-10 ml-20">
+          <div className="flex flex-col gap-10 ml-20 fixed right-36">
             <Link
-              href={""}
+              href="#main"
+              scroll={true}
               className="menu"
               data-text="Home"
               onMouseEnter={handleMouseEnter}
@@ -99,7 +107,8 @@ export default function Home() {
               </div>
             </Link>
             <Link
-              href={""}
+              href="#about"
+              scroll={true}
               className="menu"
               data-text="About"
               onMouseEnter={handleMouseEnter}
@@ -121,28 +130,111 @@ export default function Home() {
               </div>
             </Link>
           </div>
-        </section>
-        <div className="fixed bottom-10 w-full flex flex-row items-baseline justify-center">
-          <div className="flex justify-start w-full ml-20 gap-20 text-sm">
-          <h3>Social:</h3>
-          <h3>/TELEGRAM</h3>
-          <h3>/GITHUB</h3>
-          <h3>/LEETCODE</h3>
+          <div className="fixed bottom-10 w-full flex flex-row items-baseline justify-center">
+            <div className="flex justify-start w-full ml-20 gap-20 text-sm">
+              <h3>Social:</h3>
+              <Link href='https://t.me/v1daaaa' target="_blank"><h3>/TELEGRAM</h3></Link>
+              <Link href='https://github.com/V1daa' target="_blank"><h3>/GITHUB</h3></Link>
+              <Link href='https://leetcode.com/u/ViDa-la-ViDA/' target="_blank"><h3>/LEETCODE</h3></Link>
+            </div>
+            <ul className="flex flex-row gap-5 w-full mr-20 ">
+              <li
+                className={`${page === 0 ? "active" : ""} square dark:invert`}
+              ></li>
+              <li
+                className={`${page === 1 ? "active" : ""} square dark:invert`}
+              ></li>
+              <li
+                className={`${page === 2 ? "active" : ""} square dark:invert`}
+              ></li>
+            </ul>
           </div>
-          <ul className="flex flex-row gap-5 w-full mr-20 ">
-            <li
-              className={`${page === 0 ? "active" : ""} square dark:invert`}
-            ></li>
-            <li
-              className={`${page === 1 ? "active" : ""} square dark:invert`}
-            ></li>
-            <li
-              className={`${page === 2 ? "active" : ""} square dark:invert`}
-            ></li>
-          </ul>
-        </div>
-        <section className=" bg-white text-black dark:bg-black dark:text-white">
-          Section 2
+        </section>
+        <section
+          className={`bg-white text-black dark:bg-black dark:text-white flex justify-center items-center gap-52`}
+          id="about"
+        >
+          <div
+            className={` bg-gray-200 h-full w-auto flex justify-center flex-col gap-5 p-10 align-baseline ${
+              isHovered ? "opacity-0" : ""
+            }`}
+          >
+            <h1 className="text-4xl dark:text-black">DATA</h1>
+            <div className="flex flex-col gap-4 text-end w-[250px] dark:text-black">
+              <h2>
+                <span className="data">Location: &nbsp;</span> Poznań, Poland
+              </h2>
+              <h2>
+                <span className="data">Born: &nbsp;</span>
+                27.08.2002
+              </h2>
+              <h2>
+                <span className="data">Citizenship: &nbsp;</span>
+                Ukraine
+              </h2>
+              <h2>
+                <span className="data">Languages: &nbsp;</span>
+                English, Polish, Ukrainian
+              </h2>
+            </div>
+          </div>
+          <div
+            className={`bg-black text-white h-full w-[250px] flex justify-center flex-col gap-5 pl-10 align-baseline ${
+              isHovered ? "opacity-0" : ""
+            }`}
+          >
+            <h1 className="text-4xl dark:text-white">STACK</h1>
+            <div className="flex flex-row flex-wrap gap-7 items-center">
+              <Image src={"/ts.png"} alt="" width={40} height={40} />
+              <Image src={"/mongodb.png"} alt="" width={20} height={20} />
+              <Image src={"/java.png"} alt="" width={50} height={50} />
+              <Image src={"/node-js.png"} alt="" width={40} height={40} />
+              <Image src={"/figma.png"} alt="" width={20} height={20} />
+              <Image src={"/blender.png"} alt="" width={40} height={40} />
+              <Image src={"/next.png"} alt="" width={38} height={35} />
+              <Image src={"/prisma.webp"} alt="" width={35} height={35} />
+              <Image src={"/mysql.png"} alt="" width={40} height={40} />
+            </div>
+          </div>
+        </section>
+        <section className="flex items-center justify-center">
+          <div
+            className={`flex items-center justify-center flex-col gap-10 ${
+              isHovered ? "opacity-0" : ""
+            }`}
+          >
+            <h1 className="text-5xl font-bold">BOARDY</h1>
+            <div className="flex items-center justify-center gap-10">
+              <Link href="https://board-eight-blush.vercel.app/">
+                <Image
+                  src={"/board3.jpg"}
+                  className="hover:scale-125 transition-all cursor-pointer"
+                  alt=""
+                  width={700}
+                  height={700}
+                />
+              </Link>
+              <Link href="https://board-eight-blush.vercel.app/">
+                <Image
+                  src={"/board2.jpg"}
+                  className="hover:scale-125 transition-all cursor-pointer"
+                  alt=""
+                  width={700}
+                  height={700}
+                />
+              </Link>
+            </div>
+            <h2 className="max-w-[30vw] text-center">
+              Miro clone fullstack infinite whiteboard app with multicursor.
+              Created by using Liveblocks, NextJs and Zustand state manager. UI
+              was created with help of ShadcnUi. Authorization with help of
+              Clerk. And for database was chosen Convex free and realtime
+              database.{" "}
+            </h2>
+            <h2 className="text-gray-300 uppercase text-center max-w-[30vw]">
+              For more projects visit my <Link href="https://github.com/V1daa">GITHUB</Link>
+            </h2>
+          </div>
         </section>
       </div>
     </main>
